f30d18aadda316741d768fae255ff7d7138505c1
Omkar Jadhav <93418994+Omkar9911@users.noreply.github.com>
2023-07-31 18:20:54 +0530

    Add files via upload

---
+++ Task- 1 (Landing_Page)/assets/js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * navbar toggle
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("active");
+});
+
+/**
+ * show go top btn when scroll window to 500px
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 500
+    ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
+});
+let videoBtn = document.querySelectorAll("[vid-btn]");
+videoBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".controls .active").classList.remove("active");
+    btn.classList.add("active");
+    let src = btn.getAttribute("data-src");
+    document.querySelector("#video-silder").src = src;
+  });
+});
